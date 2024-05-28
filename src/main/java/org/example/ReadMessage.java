0ce75f6faaf5857520681e2795543353e2935e12
Tobias Sjöstörm <tobias.sjostrom@iths.se>
2024-05-28 07:13:55 +0200

    Test of Camels try/catch

--- src/main/java/org/example/ReadMessage.java
+++ src/main/java/org/example/ReadMessage.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 
 public class ReadMessage extends RouteBuilder {
@@ -7,9 +8,16 @@ public class ReadMessage extends RouteBuilder {
     @Override
     public void configure() {
         from("activemq:queue:my_queue")
-                .process(exchange -> {
-                    String message = exchange.getIn().getBody(String.class);
-                    System.out.println("Message received: " + message);
+                .doTry()
+                    .process(exchange -> {
+                        String message = exchange.getIn().getBody(String.class);
+                        System.out.println("Message received: " + message);
+                        //throw new Exception("Test exception");
+                    })
+                .doCatch(Exception.class)
+                    .process(exchange -> {
+                        Exception e = exchange.getProperty(Exchange.EXCEPTION_CAUGHT, Exception.class);
+                        System.out.println("Error processing message: " + e.getMessage());
                 });
     }
 }
